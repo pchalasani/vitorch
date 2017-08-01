36745915a9528088dfc4484ad1f31868e15fb882
Prasad Chalasani <pchalasani@mediamath.com>
2017-08-01 11:58:04 -0400

    docs/conf.py -- mock numpy etc

--- docs/conf.py
+++ docs/conf.py
@@ -287,3 +287,13 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.linalg', 'scipy.signal', 'pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
